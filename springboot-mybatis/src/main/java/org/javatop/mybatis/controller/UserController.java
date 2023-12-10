97be4f69ee6db61bd4037b12e3f15c9576a97ca8
gaoziman <2942894660@qq.com>
2023-12-10 13:42:19 +0800

    添加打包插件

--- springboot-mybatis/src/main/java/org/javatop/mybatis/controller/UserController.java
+++ springboot-mybatis/src/main/java/org/javatop/mybatis/controller/UserController.java
@@ -1,4 +1,5 @@
 package org.javatop.mybatis.controller;
+
 import org.javatop.mybatis.pojo.User;
 import org.javatop.mybatis.service.impl.UserServiceImpl;
 import org.springframework.web.bind.annotation.*;
@@ -6,34 +7,35 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.annotation.Resource;
 
 /**
-* @author : Leo
-* @date  Tue Nov 21 15:37:39 CST 2023
-* @version 1.0
-* @description : (user)表控制层
-*/
+ * @author : Leo
+ * @version 1.0
+ * @date Tue Nov 21 15:37:39 CST 2023
+ * @description : (user)表控制层
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
     /**
-    * 服务对象
-    */
+     * 服务对象
+     */
     @Resource
     private UserServiceImpl userServiceImpl;
 
     /**
-    * 通过主键查询单条数据
-    *
-    * @param id 主键
-    * @return 单条数据
-    */
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
     @GetMapping("/{id}")
     public User getById(@PathVariable Integer id) {
-    return userServiceImpl.selectById(id);
+        return userServiceImpl.selectById(id);
     }
 
 
     /**
      * 通过手机号查找用户
+     *
      * @param phone 手机号
      * @return 用户
      */
